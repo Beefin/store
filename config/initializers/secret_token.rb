cfde2d1dbbf9e90a460cc56bc9b18604182ab450
Ethan Steininger <beefinethan@gmail.com>
2013-06-10 20:06:17 -0400

    eCommerce web app I started
    Be sure to comment/document your edits

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'b50e47dac699a9fe036ca14f597dd544016557739701cca618ce72c545d9044d0099eadfcb76fcd4932ba6998dbfdb3a33efd8efce7cf465bcdd24844cec7bbe'
